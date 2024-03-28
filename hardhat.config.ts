a9ab1da35fab4cf9b2280f9fe8fb0e003d53e4ed
Scott Tsai <scott.tsai@thundertoken.tw>
2024-03-28 21:35:38 +0800

    hardhat.config.ts: cleanup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "tt-hardhat";
-//import * as dotenv from "dotenv";
-//dotenv.config();
 
 const config: HardhatUserConfig = {
-   //solidity: { compilers: [ { version: "0.8.25" } ] },
    solidity: { version: "0.8.25" },
    networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
       blockGasLimit: 141438723,
     },
-   "thunder-testnet0": {
-      gasPrice: "auto",
-      url: "https://testnet-rpc.thundercore.com",
-      chainId: 18,
-      accounts: [process.env.KEY],
-  },
   }
 };
 
